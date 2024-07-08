3f1c9ff8409046c3c7e3e50e1580e8344a7187d2
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2024-07-08 23:08:02 +0800

    Updated to part 6 of Django tutorial

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    # Available genres and books with a specific word in them.
+    num_specific_books = Book.objects.filter(title__contains='Book').count()
+    num_specific_genres = Genre.objects.filter(name__contains='Fantasy').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_specific_genres' : num_specific_genres,
+        'num_specific_books' : num_specific_books,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
